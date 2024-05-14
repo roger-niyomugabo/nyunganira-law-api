5deb293ea11efc9f99269aceb74687be0accca3c
roger-niyomugabo <rogerniyomugabo3@gmail.com>
2024-05-14 13:44:52 +0200

    users login

--- src/routes/v1/users_resource.ts
+++ src/routes/v1/users_resource.ts
@@ -1,11 +1,12 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import express, { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
+import { User } from '../../db/models';
+import { check } from '../../utils/bcrypt';
 import { validate } from '../../middleware/middleware';
 import { asyncMiddleware } from '../../middleware/error_middleware';
 import output from '../../utils/response';
 import { sign } from '../../utils/jwt';
-import config from '../../config';
-
 const router = express.Router();
 
 // users login validations
@@ -18,12 +19,27 @@ const usersLoginValidations = Joi.object({
 router.post('/login', validate(usersLoginValidations), asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
-    if (email !== config.ADMIN_EMAIL || password !== config.ADMIN_PASSWORD) {
+    const user = await User.findOne({ where: { email } });
+    if (!user || !['lawyer', 'client'].includes(user.role)) {
+        return output(res, 404, 'Email not registered', null, 'NOT_FOUND_ERROR');
+    }
+
+    const isMatch = check(user.password, password);
+    if (!isMatch) {
         return output(res, 401, 'Incorrect email or password', null, 'AUTHENTICATION_ERROR');
     }
-    const token = sign({ role: 'admin' });
-    return output(res, 200, 'Logged in successfully', token, null);
+    user.password = undefined;
 
+    if (user.role === 'lawyer') {
+        const token = sign({ lawyerId: user.id, role: user.role });
+        return output(res, 200, 'Logged in successfully', { user, token }, null);
+    }
+    if (user.role === 'client') {
+        const token = sign({ clientId: user.id, role: user.role });
+        return output(res, 200, 'Logged in successfully', { user, token }, null);
+    }
+
+    return output(res, 401, 'Invalid login cridentials', null, 'AUTHENTICATION_ERROR');
 })
 );
 
