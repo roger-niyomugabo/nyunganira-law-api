2d16b4fc6dee943e74d0933ac440a9e101ce2464
roger-niyomugabo <rogerniyomugabo3@gmail.com>
2024-05-27 21:34:58 +0200

    full payment

--- src/routes/v1/payment_resource.ts
+++ src/routes/v1/payment_resource.ts
@@ -1,43 +1,26 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import express, { NextFunction, Request, Response } from 'express';
 import { asyncMiddleware } from '../../middleware/error_middleware';
-import { paypackConfig } from '../../utils/paypackConfig';
 import output from '../../utils/response';
-import { CaseRequest, Lawyer, Payment, User } from '../../db/models';
+import { CaseRequest, User } from '../../db/models';
 import { isClient, isLawyer } from '../../middleware/access_middleware';
+import { processPayment } from '../../utils/processPayment';
 
 const router = express.Router({ mergeParams: true });
 
-// Pay
-router.post('/', isClient, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
-    const { clientId } = req.user;
-    const { caseRequestId } = req.params;
-
-    const cases = await CaseRequest.findOne({ where: { id: caseRequestId }, include: [{ model: Lawyer, as: 'lawyer', include: [{ model: User, as: 'user' }] }] });
-    if (!cases) {
-        return output(res, 404, 'No case rquest to pay for', null, 'NOT_FOUND_ERROR');
-    }
-    const amount = cases.downPayment;
-    const number = cases?.lawyer?.user?.phoneNumber;
-    const response = await paypackConfig.cashin(amount, number);
-    if (response.data.status === 'pending') {
-        const newPayment = await Payment.create({
-            amount,
-            type: response.data.kind,
-            status: response.data.status,
-            userId: clientId,
-            refId: response.data.ref,
-            provider: response.data.provider,
-            fee: response.data.fee,
-            caseRequestId,
-        });
-
-        return output(res, 200, 'payment request sent successful', newPayment, null);
-    }
-    return output(res, 200, 'payment request sent successful', null, null);
+// down payment
+router.post('/downPayment', isClient, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+    await processPayment(req, res, next, 'downPayment');
 })
 );
 
+// full payment
+router.post('/fullPayment', isClient, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+    await processPayment(req, res, next, 'fullPayment');
+})
+);
+
+// requesting full payment
 router.patch('/request', isLawyer, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
     const { lawyerId } = req.user;
     const { caseRequestId } = req.params;
---
+++ src/utils/processPayment.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import output from '../utils/response';
+import { CaseRequest, Lawyer, Payment, User } from '../db/models';
+import { paypackConfig } from './paypackConfig';
+
+export const processPayment = async (req: Request, res: Response, next: NextFunction, paymentType: 'downPayment' | 'fullPayment') => {
+    const { clientId } = req.user;
+    const { caseRequestId } = req.params;
+
+    const caseRequest = await CaseRequest.findOne({
+        where: { id: caseRequestId },
+        include: [{ model: Lawyer, as: 'lawyer',
+            include: [{ model: User, as: 'user' }],
+        }],
+    });
+
+    if (!caseRequest) {
+        return output(res, 404, 'No case request to pay for', null, 'NOT_FOUND_ERROR');
+    }
+
+    const amount = paymentType === 'downPayment' ? caseRequest.downPayment : caseRequest.fullPayment;
+    const number = caseRequest?.lawyer?.user?.phoneNumber;
+
+    if (!number) {
+        return output(res, 400, 'Lawyer phone number not found', null, 'BAD_REQUEST_ERROR');
+    }
+
+    const response = await paypackConfig.cashin(amount, number);
+
+    if (response.data.status === 'pending') {
+        const newPayment = await Payment.create({
+            amount,
+            type: response.data.kind,
+            status: response.data.status,
+            userId: clientId,
+            refId: response.data.ref,
+            provider: response.data.provider,
+            fee: response.data.fee,
+            caseRequestId,
+        });
+
+        return output(res, 200, 'Payment request sent successfully', newPayment, null);
+    }
+
+    return output(res, 200, 'Payment request sent successfully', null, null);
+};
